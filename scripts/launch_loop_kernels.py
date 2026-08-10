bfd517ee9cd9c413a11589e1e4b7e4c27f1f671d
amerameryou1-blip <amerameryou1-blip@users.noreply.github.com>
2026-08-10 09:45:23 +0000

    HD workers rec_every=2 (+50% data/h at same sim cost)

--- scripts/launch_loop_kernels.py
+++ scripts/launch_loop_kernels.py
@@ -45,7 +45,7 @@ V2_WORKER_BOOT = WORKER_BOOT.replace(
     'os.environ["EP_PER_SHARD"] = "10"',
     'os.environ["V2"] = "1"\nos.environ["V2_EP"] = "6"\n'
     'os.environ["FLUSH_S"] = "1200"\nos.environ["V2_SIZE"] = "192"\n'
-    'os.environ["REC_EVERY"] = "3"')
+    'os.environ["REC_EVERY"] = "2"')
 
 TRAINER_BOOT = '''import os, sys, subprocess, time
 os.environ.setdefault("HF_TOKEN", "@@HF@@")
